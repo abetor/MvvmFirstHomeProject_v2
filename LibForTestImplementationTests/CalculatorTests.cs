6053d971f6b95769a11207e139ee4082f470dfe4
abetor <Molaytuh@gmail.com>
2018-04-08 00:27:14 +0500

    + Начало работы с тестированием

---
+++ LibForTestImplementationTests/CalculatorTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LibForTestImplementation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibForTestImplementation.Tests
+{
+    [TestClass()]
+    public class CalculatorTests
+    {
+        [TestMethod()]
+        public void AddShouldReturnSumOfValues()
+        {
+            var calculator = new Calculator();
+            var result = calculator.Add(2, 3);
+            Assert.AreEqual(5,result);
+        }
+    }
+}
---
+++ TestsLibForXUnit/CalculatorTestsXUnit.cs
@@ -0,0 +1,21 @@
+﻿using LibForTestImplementation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace TestsLibForXUnit
+{
+    public class CalculatorTestsXUnit
+    {
+        [Fact]
+        public void AddShouldReturnSumOfValues()
+        {
+            var calculator = new Calculator();
+            var result = calculator.Add(2, 3);
+            Assert.Equal(5, result);
+        }
+    }
+}
